901ed7acc6650191aff55051bcc45a1b91204a51
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2021-11-09 21:18:19 +0100

    express aplication up and running

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+require("dotenv").config();
+const path = require("path");
+const hbs = require("hbs");
 const express = require("express");
 
+const port = process.env.PORT;
 const app = express();
+
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+app.set("view engine", "hbs");
+app.set("views", viewPath);
+hbs.registerPartials(partialsPath);
+
+app.use(express.static(publicDirectoryPath));
+
+app.get("/", async (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
