21c70a77b0325fac042eae49f109bcb3a0a19896
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2021-11-10 19:24:21 +0100

    socket io setup and serverside connecting

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
 const path = require("path");
 const hbs = require("hbs");
+const http = require("http");
 const express = require("express");
+const socketio = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT;
-const app = express();
-
 const publicDirectoryPath = path.join(__dirname, "../public");
 const viewPath = path.join(__dirname, "../templates/views");
 const partialsPath = path.join(__dirname, "../templates/partials");
@@ -20,6 +24,10 @@ app.get("/", async (req, res) => {
   res.render("index");
 });
 
-app.listen(port, () => {
+io.on("connection", () => {
+  console.log("New websocket connection");
+});
+
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
