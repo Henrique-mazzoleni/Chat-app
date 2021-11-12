8771a6b74bb2f2378d00e9001b42a79f7ed233e0
Henrique-mazzoleni <henrique.mazzoleni@gmail.com>
2021-11-12 12:37:50 +0100

    buttons states functional

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,22 +1,35 @@
 const socket = io();
 
+const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
+
 socket.on("message", (message) => {
   console.log(message);
 });
 
-document.querySelector("form").addEventListener("submit", (e) => {
+$messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  const input = e.target.elements.message;
-  const message = input.value;
+
+  $messageFormButton.setAttribute("disabled", "disabled");
+
+  const message = $messageFormInput.value;
+
   socket.emit("sendMessage", message, (error) => {
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+
     if (error) return console.log(error);
     console.log("message delivered");
   });
-  input.value = "";
 });
 
-document.querySelector("#send-location").addEventListener("click", (e) => {
+$sendLocationButton.addEventListener("click", (e) => {
   if (!navigator.geolocation) return alert("not suported by browser");
+
+  $sendLocationButton.setAttribute("disabled", "disabled");
 
   navigator.geolocation.getCurrentPosition((position) => {
     socket.emit(
@@ -26,6 +39,7 @@ document.querySelector("#send-location").addEventListener("click", (e) => {
         longitude: position.coords.longitude,
       },
       () => {
+        $sendLocationButton.removeAttribute("disabled");
         console.log("Location shared!");
       }
     );
